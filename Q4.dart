3cb0909b382fe13eb131d8bc3d7344ae16742b00
tamseel2217 <93148385+tamseel2217@users.noreply.github.com>
2022-09-30 13:26:05 -0700

    Add files via upload
    Question 4

---
+++ Q4.dart
@@ -0,0 +1,19 @@
+void main() {
+  print("Tamseel Ahmed Khan");
+  print("Assignment 1 = Qustions 4");
+  print("If the marks of Robert in three subjects are 78,45 and 62 respectively");
+  print("(each out of 100 ), write a program to calculate his total marks and");
+print("percentage marks. Print his name, marks of all three subjects,");
+  print("total marks and percentage.");
+  var mark1=78;
+    var mark2=45;
+    var mark3=62;
+  print("Mark1 = $mark1");
+  print("Mark2 = $mark2");
+print("Mark3 = $mark3");
+print("Total Marks = 100");
+  int obtmrk = mark1+mark2+mark3;
+  print("Obtain Marks = $obtmrk");
+  double percentage = (obtmrk*100)/300;
+  print("Percentage = $percentage");
+}
