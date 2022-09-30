51993196d6276ecb7650568b0c1b1ab908b47249
tamseel2217 <93148385+tamseel2217@users.noreply.github.com>
2022-09-30 10:28:07 -0700

    Add files via upload
    Question 3

---
+++ Q3.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Assignment 1 = Qustions 3");
+  print("Write a program create two integer variables 'a' and 'b'");
+  print("and assign them any number and then check if both the conditions");
+print("'a < 50' and 'a < b' are true. Display the result, now check if at-least one of the conditions 'a < 50' and 'a < b' is true");
+print("of the conditions 'a < 50' and 'a < b' is true");
+  int a = 30;
+  print("a = $a");
+  int b = 25;
+  print ("b = $b");
+  //var result1 ="a < 50"
+  print(!(a<50));
+  print(!(a<50==a<b));
+  }
