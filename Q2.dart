e5a74bdf1696297f51faae8639609c62932602cf
tamseel2217 <93148385+tamseel2217@users.noreply.github.com>
2022-09-30 08:57:19 -0700

    Add files via upload
    Question 2

---
+++ Q2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num = 7;
+  print("Number = $num");
+  int num1 = num + 8;
+  print("Addition = $num1");
+  var num2 = num1/3;
+  print("Divide Result = $num2");
+  var modulus = num2 % 5;
+  print("Modulus = $modulus");
+  var multiply = modulus * 5;
+  print("Multiply = $multiply");
+}
